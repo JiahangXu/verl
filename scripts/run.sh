994395bd3eb58c8084893fc87f0203e3a96d1bc0
jiahangxu <jiahangxu@microsoft.com>
2025-03-10 07:47:09 +0000

    update run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,50 @@
+set -x
+
+gsm8k_train_path=$HOME/data/compressed_gsm8k/train.parquet
+gsm8k_test_path=$HOME/data/compressed_gsm8k/test.parquet
+
+train_files=$gsm8k_train_path
+test_files=$gsm8k_test_path
+
+state_dict=gsm8k_think_token_06_hf
+
+python3 -m verl.trainer.main_ppo \
+    data.train_files="$train_files" \
+    data.val_files="$test_files" \
+    data.train_batch_size=1024 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=512 \
+    actor_rollout_ref.model.path=/home/aiscuser/Qwen2.5-7B-Instruct \
+    actor_rollout_ref.model.state_dict=/home/aiscuser/$state_dict \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    critic.optim.lr=1e-5 \
+    critic.model.use_remove_padding=True \
+    critic.model.path=/home/aiscuser/Qwen2.5-7B-Instruct \
+    critic.model.state_dict=/home/aiscuser/$state_dict \
+    critic.model.enable_gradient_checkpointing=True \
+    critic.ppo_micro_batch_size_per_gpu=32 \
+    critic.model.fsdp_config.param_offload=True \
+    critic.model.fsdp_config.optimizer_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='thinking_lingua_ppo' \
+    trainer.experiment_name=${state_dict}_0310 \
+    +trainer.val_before_train=False \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=1 \
+    trainer.total_epochs=15 $@
